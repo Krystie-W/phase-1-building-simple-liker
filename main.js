31d61bd8a1ad5ac58a0be83add9baab8401aeef8
Krystie-W <tsukio.scoring@gmail.com>
2021-11-17 11:55:49 +1100

    Completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 //Other targets
 //Had to change the numeric ids in the HTML to text because it wasn't working for some reason
-//Come back to retry ID numbers again later
 const byronsHeart = document.querySelector('article#byron footer ul li span');
 const rashaunsHeart = document.querySelector('article#rashaun footer ul li span');
 const mattsHeart = document.querySelector('article#matt footer ul li span');
@@ -15,59 +14,37 @@ const errorMessage = document.querySelector('#modal-message');
 //Dom content loaded
 window.addEventListener("DOM content loaded", start());
 
-//Add an event listener for the heart click - event listener is added to 
-//each heart separately - see if there is a way I can handle this in a more concise way at the end...
 function start () {
     byronsHeart.addEventListener('click', () => {
       allHeartFunctionality(byronsHeart);
-      })
-    //When the heart is already red, add an event listener if the user clicks again to remove the activated heart class
-    clickHeart(byronsHeart);
-    rashaunsHeart.addEventListener('click', (event) => {
-    //STILL DO DO - Update each of the below to use the same functions once Byron's heart is working
-    clickHeart(rashaunsHeart);
+      })   
+    rashaunsHeart.addEventListener('click', () => {
+    allHeartFunctionality(rashaunsHeart);
     })
-    mattsHeart.addEventListener('click', (event) => {
-    clickHeart(mattsHeart);
+    mattsHeart.addEventListener('click', () => {
+      allHeartFunctionality(mattsHeart);
     })
-    yunchusHeart.addEventListener('click', (event) => {
-    clickHeart(yunchusHeart);
+    yunchusHeart.addEventListener('click', () => {
+      allHeartFunctionality(yunchusHeart);
     })
-    husseinsHeart.addEventListener('click', (event) => {
-    clickHeart(husseinsHeart);
+    husseinsHeart.addEventListener('click', () => {
+      allHeartFunctionality(husseinsHeart);
     });
 }
 
-/*This is the functionality if a user clicks a heart after it is already a full heart
-NEED TO NOT HAVE THE LISTENER ON WHILE THE HEART IS FULL - it's running the server call when the user clicks 
-and the heart is full*/
-function clickHeart(heart) {
-  heart.addEventListener('click', () => {
-    if(heart.innerHTML === FULL_HEART) {
-      heart.classList.remove('activated-heart')
-      heart.innerHTML = EMPTY_HEART;
-    }
-  });
-};
-
-//This is the functionality to call the server and handle the response
-//Still having issues with the error message - success is working ok - can't get the catch code block to run
-function allHeartFunctionality(heart) {
-  mimicServerCall()
-  .then(function(data) {
-  //If the click was successful, change the heart to a full heart, add the activated-heart class to make the heart appear red
-    try {
-      console.log(data.toString());
-        if (data.toString() !== 'Random server error. Try again.') {
+//This is the functionality to update the heart
+async function allHeartFunctionality(heart) {
+  if(heart.innerHTML === FULL_HEART) {
+    heart.classList.remove('activated-heart')
+    heart.innerHTML = EMPTY_HEART;
+  }else try{
+  const data = await mimicServerCall();
           firstHeartUpdate(heart);
-        }
-  //If the click was unsuccessful: Unhide the error modal and pass the message into the error modal      
-    } catch (error) {
-        console.log('error!');
+       }catch (error) {
         errorBehaviour(error);
       }
-  });
-};
+  }
+
 
   //This is the functionality to first make the heart red when there is a successful response
 function firstHeartUpdate (heart) {
@@ -80,7 +57,7 @@ function errorBehaviour(error) {
   console.log(error);
   errorPopUp.classList.remove('hidden');
   errorMessage.innerHTML = 'Random server error. Try again';
-  //STILL TO DO Set a timeout on the error modal to disapear (insert .hidden class again) after 3 secs (3000)
+  setTimeout(function(){ errorPopUp.classList.add('hidden'); }, 3000);
 }
 
 
